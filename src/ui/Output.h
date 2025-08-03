386037e206a32a2cc7d6734fe2709bc972c070ec
uintmax <149969206+uintmax@users.noreply.github.com>
2025-08-03 22:55:53 +0200

    Add move contructor for Output class

--- src/ui/Output.h
+++ src/ui/Output.h
@@ -9,7 +9,10 @@
 #include "Icons.h"
 
 namespace ui {
-    // TODO: Free resources in deconstructor
+    /**
+     * GUI element that represents a Sway output
+     * Draws the output name, an icon and sets the background color depending on its state
+     */
     class Output {
     public:
         Output(const SwayOutput &sway_output, SDL_Renderer *renderer, TTF_Font *font): sway_info(sway_output),
@@ -29,6 +32,22 @@ namespace ui {
             if (!icon_texture)
                 throw std::runtime_error("Could not load icon");
         }
+
+        // TODO: Check out std::exchange
+        Output(Output &&other) noexcept : sway_info(std::move(other.sway_info)), renderer(other.renderer),
+                                          font(other.font),
+                                          text_surface(other.text_surface),
+                                          text_texture(other.text_texture),
+                                          is_embedded_display(other.is_embedded_display),
+                                          icon_texture(other.icon_texture) {
+            other.renderer = nullptr;
+            other.font = nullptr;
+            other.text_surface = nullptr;
+            other.text_texture = nullptr;
+            other.icon_texture = nullptr;
+        }
+
+        ~Output();
 
         void draw(float x, float y, float w, float h) const;
 
